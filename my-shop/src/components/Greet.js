d5274e0703cf5742c4a6abffb8d611483f0b8e85
John Paul Ico <jpi.freelancer@gmail.com>
2023-08-12 09:10:35 +0800

    part 1

---
+++ my-shop/src/components/Greet.js
@@ -0,0 +1,9 @@
+// Functional Components
+
+import React from "react";
+// function Greet(){
+//     return <h1>Hello JPI</h1>
+// }
+// export Greet = () => <h1>Hello JPI</h1>
+const Greet = () => <h1>Hello JPI</h1>
+export default Greet
---
+++ my-shop/src/components/Welcome.js
@@ -0,0 +1,8 @@
+//Class Components
+import React, {Component} from "react";
+class Welcome extends Component{
+    render(){
+        return <h1>Welcome to Class Component</h1>
+    }
+}
+export default Welcome
